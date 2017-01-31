dd5ab4329c533fd904a36419f6b545c1de873528
yaseenomer <yaseeno373@gmail.com>
2017-01-31 09:11:55 +0300

    Add files via upload

---
+++ mysite/polls/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+
+# Create your views here.
+def index(request):
+    latest_question_list= Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request,question_id):
+    return HttpResponse("YOU 're looking at question %s."%question_id)
+def results(request,question_id):
+    response="you ar looking at the result of question %s."
+    return HttpResponse(response %question_id)
+
+def vote(request, question_id):
+    return HttpResponse("your voting on guestion %s."%question_id)
